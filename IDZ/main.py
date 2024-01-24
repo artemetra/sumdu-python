adfdb5ad85d6329f4836fcce0f012efc965a9b21
artemetra <artemlukin15@gmail.com>
2024-01-24 12:58:44 +0100

    style tweaks, columns

--- IDZ/main.py
+++ IDZ/main.py
@@ -3,11 +3,12 @@ from PIL import Image
 
 
 class CV(FPDF):
-
     image_x = 10  # X-координата верхнього лівого кута фото
     image_y = 10  # Y-координата верхнього лівого кута фото
 
     offset = 100 # відступ для другої колонки
+
+    typeface = "Times"
     
     def __init__(self):
         super().__init__()
@@ -15,17 +16,17 @@ class CV(FPDF):
         self.has_education = False
 
     def header(self):
-        self.set_font("Arial", "B", 12)
+        self.set_font(self.typeface, "B", 14)
         self.cell(0, 10, "Curriculum Vitae", 0, 1, "C")
 
     def footer(self):
         self.set_y(-15)
-        self.set_font("Arial", "I", 8)
+        self.set_font(self.typeface, "I", 8)
         self.cell(0, 10, f"Page {self.page_no()}", 0, 0, "C")
 
     def add_personal_info(self, name, email, address=None, phone=None):
-        self.set_font("Arial", "B", 12)
-        self.set_font("Arial", "", 12)
+        self.set_font(self.typeface, "B", 12)
+        self.set_font(self.typeface, "", 12)
         self.cell(0, 10, f"{name}", 0, 1, "R")
         if address is not None:
             self.cell(0, 10, f"{address}", 0, 1, "R")
@@ -35,23 +36,32 @@ class CV(FPDF):
         self.ln(10)
 
     def add_education(self, degree, school, year):
-        self.cell(self.offset)
-        
+        self.set_x(self.offset)
+
+        # Education header        
         if not self.has_education:
-            self.set_font("Arial", "B", 12)
+            self.set_font(self.typeface, "B", 14)
             self.cell(0, 10, "Education", 0, 1, "L")
             self.has_education = True
         
-        self.set_font("Arial", "", 12)
-        self.cell(0, 10, f"Degree: {degree}", 0, 1, "L")
-        self.cell(0, 10, f"School: {school}", 0, 1, "L")
-        self.cell(0, 10, f"Year: {year}", 0, 1, "L")
+        self.set_font(self.typeface, "I", 12)
+        self.set_x(self.offset)
+        self.cell(0, 10, degree, 0, 1, "L")
+        self.set_font("")
+        self.set_x(self.offset)
+        self.cell(0, 10, f"{school}, {year}", 0, 1, "L")
+        # self.set_x(self.offset)
+        # self.cell(0, 10, f"Year: {year}", 0, 1, "L")
         self.ln(10)
 
     def add_experience(self, position, company, year):
-        self.set_font("Arial", "B", 12)
-        self.cell(0, 10, "Work Experience", 0, 1, "L")
-        self.set_font("Arial", "", 12)
+        self.set_y(70)
+        # Work Experience header
+        if not self.has_experience:
+            self.set_font(self.typeface, "B", 14)
+            self.cell(0, 10, "Work Experience", 0, 1, "L")
+            self.has_experience = True
+        self.set_font(self.typeface, "", 12)
         self.cell(0, 10, f"Position: {position}", 0, 1, "L")
         self.cell(0, 10, f"Company: {company}", 0, 1, "L")
         self.cell(0, 10, f"Year: {year}", 0, 1, "L")
